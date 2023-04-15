383454c4369ba319a076c6a568effaab8572303d
CallumBlair <s5330191@bournemouth.ac.uk>
2023-04-15 17:21:55 +0100

    Node Id bug fixed
    Multiple nodes were trying to be created with the same Id, this has been resolved

--- aStarModule.py
+++ aStarModule.py
@@ -7,11 +7,48 @@ testGrid = [[0,0,1,0,0,0,0,0,0],
             [0,0,0,1,1,1,1,1,1],
             [0,0,0,0,0,1,0,0,0],
             [0,0,0,0,0,1,0,0,0],
-            [0,0,0,0,0,1,0,0,0],
             [0,0,0,0,0,0,0,0,0],
             [0,0,0,0,0,1,0,0,0]]
+
+testGrid2 = [[0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+             [0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+             [0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+             [0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1,1,1,0,1,0,0,0,0,0,0,0],
+             [0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0],
+             [0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0],
+             [0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0],
+             [0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1,1,0,0,1,1,1,1,0,0,0,0],
+             [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+             [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0],
+             [0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+             [0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+             [0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+             [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+             [0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+             [1,1,1,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+             [0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0],
+             [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0],
+             [0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0],
+             [0,1,1,1,1,1,1,1,0,1,0,1,0,0,0,1,0,0,1,0,0,0,1,0,0,0,0,0],
+             [0,0,0,0,0,0,0,1,0,1,0,1,0,0,0,1,0,0,1,0,0,0,1,0,0,0,0,0],
+             [0,0,0,0,0,0,0,1,0,1,0,1,0,0,0,1,0,0,1,0,0,0,1,0,0,0,0,0],
+             [1,1,1,1,1,1,0,1,0,1,0,1,0,0,0,1,0,0,1,0,0,0,1,0,0,0,0,0],
+             [0,0,0,0,0,1,0,1,0,0,0,1,0,0,0,1,0,0,1,0,0,0,1,0,0,0,0,0],
+             [0,1,1,1,0,1,0,1,0,0,0,1,0,0,0,1,0,0,1,0,0,0,1,0,0,0,0,0],
+             [0,0,0,1,0,1,0,1,0,0,0,1,0,0,0,1,0,0,1,0,0,0,1,0,0,0,0,0],
+             [0,0,0,1,0,1,0,1,0,0,0,1,0,0,0,1,0,0,1,0,0,0,1,0,0,0,0,0],
+             [0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0],              
+            ]
+
+
 startNode = [0,0]
-targetNode = [7,6]
+#targetNode = [7,8]
+targetNode = [0,32]
 
 openList = []
 closedList = []
@@ -21,8 +58,8 @@ class Node:
         self.id = str(ident)
         self.x = cord[0]
         self.y = cord[1]
-        self.h = 0
-        self.distStart = -1
+        self.h = -1
+        self.distStart = None
         self.parent = None
         if neighbor == None:
             self.neighbors = []
@@ -50,6 +87,7 @@ class Node:
         string += ", parent: " + str(self.parent)
         string += ", neighbors" + str(self.neighbors)
         return string
+
 
 
 class graph:
@@ -88,7 +126,7 @@ class graph:
         for x in range(len(grid)):
             for y in range(len(grid[x])):
                 currentCord = [x,y]
-                currentId = str(x) + str(y)
+                currentId = "00000" + str(x) + "00000" + str(y)
                 #print("id" + str(currentId))
                 if grid[x][y] == 0:
                     self.addNode(currentCord,currentId)
@@ -114,21 +152,126 @@ class graph:
         neighborCord.append([x+1,y-1])
         for cord in neighborCord:
             if cord[0] >= 0 and cord[0] <= maxX and cord[1] >= 0 and cord[1] <= maxY:
-                neighborId.append(str(cord[0]) + str(cord[1]))
+                neighborId.append("00000" + str(cord[0]) + "00000" + str(cord[1]))
         return neighborId
                 
+class aStar():
+
+    def __init__(self,graph, startNodeId, targetNodeId):
+        self.graph = graph
+        self.startId = startNodeId
+        self.targetId = targetNodeId
+        self.start = graph.getNode(startNodeId)
+        self.target = graph.getNode(targetNodeId)
+        self.open = []
+        self.closed = []
+        self.steps = 0
         
+    def manhattan(self, node1, node2):
+        return abs(node1.x - node2.x) + abs(node1.y - node2.y)
+
+    def distance(self, parent, child):
+            distance = parent.distStart + 1
+
+            
+            if child.parent == None:
+                child.parent = parent
+            if child.distStart == None:
+                child.distStart = distance
+
+            
+            if distance < child.distStart:
+                child.parent = parent
+                child.distStart = distance
+                return distance
+            return child.distStart
+                    
+    def hValue(self, parent, child, target):
+        h = self.distance(parent, child) + self.manhattan(child, target)
+        child.h = h
+        return h
+
+    def openedEmpty(self):
+        if self.open != []:
+            return True
+        else:
+            return False
+    def removeOpened(self):
+        lowestNode = self.open[0]
+        for x in self.open:
+            if x.h < lowestNode.h:
+                lowestNode = x
+        return lowestNode
+        
+    def findPath(self):
+        self.open.append(self.start)
+        self.start.distStart = 0
+        self.start.h = self.manhattan(self.start, self.target)
+        notComplete = True
+        
+        while self.openedEmpty() and notComplete:
+            currentNode = self.removeOpened()
+            #print(currentNode.details())
+            self.open.remove(currentNode)
+            self.closed.append(currentNode)
+            if currentNode == self.target:
+                notComplete = False
+                #print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+
+            for nodes in currentNode.neighbors:
+                #print(nodes)
+                node = self.graph.getNode(nodes)
+                if node not in self.closed:
+                    if node not in self.open:
+                        self.open.append(node)
+                    cost = self.hValue(currentNode, node, self.target)
+                    #print("COST" + str(cost))
+        #print(notComplete)
+        route = []
+        nodes = self.target
+        routeIncomplete = True
+        route.append(nodes.getId())
+        while routeIncomplete:
+            #print(nodes.details())
+            route.append(nodes.parent.getId())
+            nodes = nodes.parent
+            if nodes == self.start:
+                routeIncomplete = False
+        #print(route)
+        return(route)
+
+    def displayImage(self, grid, route):
+        array = []
+        for stops in route:
+                array.append([self.graph.getNode(stops).x,self.graph.getNode(stops).y])
+        for z in array:
+            grid[z[0]][z[1]] = 3
+##        for g in range(len(grid)):
+##            af = ""
+##            for k in range(len(grid[1])):
+##                if grid[g][k] == 0:
+##                    af = af + " "
+##                else:
+##                    af = af + str(grid[g][k])
+##            print(af)
+        for p in grid:
+            print(p)
+        
+
 graph = graph()
 ##graph.addNode([1,1],1)
 ##graph.addNode([1,2],2)
 ##graph.addEdge(1,2)
 ##graph.addEdge(1,2)
 ##print(graph.getNode("1").details())
-graph.parseGrid(testGrid)
-print(graph.getNodes())
-astar = aStar(graph, "00", "76")
-print(astar.manhattan(graph.getNode("00"),graph.getNode("76")))
-
+graph.parseGrid(testGrid2)
+#print(graph.getNodes())
+#str(00000)
+#astar = aStar(graph, "000000000000", "000007000008")
+astar = aStar(graph, "000000000000", "0000032000000")
+#print(astar.manhattan(graph.getNode("00"),graph.getNode("78")))
+route = astar.findPath()
+astar.displayImage(testGrid2,route)
 #graph.parseNeighbors([5,5], testGrid)
 
 ##graph = graph()
